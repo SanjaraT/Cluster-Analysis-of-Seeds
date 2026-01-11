d40dcf8e3016444c33e28fc6d5edfa4d478c485e
sanjaraT <sanjaratunola@gmail.com>
2026-01-11 19:48:07 +0600

    Data Visualization

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ import seaborn as sns
 
 cols = ["Area","Perimeter","Compactness","Length","Width","Coefficient","Groove","Class"]
 df = pd.read_csv("seeds_dataset.txt",names = cols, sep = "\s+")
-print(df.head())
+# print(df.head())
+
+df["Class"].value_counts().plot(kind="bar")
+plt.xlabel("Seed Class")
+plt.ylabel("Counts")
+plt.title("Class Distribution")
+# plt.show()
+
+sns.pairplot(df, hue ="Class",diag_kind="hist")
+# plt.show()
+
+plt.figure(figsize=(12,8))
+sns.heatmap(df.drop("Class",axis = 1).corr(),
+            annot= True,
+            cmap= "coolwarm",
+            fmt = ".2f")
+plt.title("Correlation Matrix")
+plt.show()
