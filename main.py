a267842ed397c3a13ca12238b68c4fa4e2e42f70
sanjaraT <sanjaratunola@gmail.com>
2026-01-12 19:58:16 +0600

    KMeans clustering

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 
 
 cols = ["Area","Perimeter","Compactness","Length","Width","Coefficient","Groove","Class"]
@@ -31,5 +32,42 @@ y_true = df["Class"]
 
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
-df_scaled = pd.DataFrame(X_scaled, columns=X.columns)
-print(df_scaled.head())
+# df_scaled = pd.DataFrame(X_scaled, columns=X.columns)
+# print(df_scaled.head())
+
+kmeans = KMeans(n_clusters = 3, random_state = 42)
+clusters = kmeans.fit_predict(X_scaled)
+df["cluster"] = clusters
+
+comparison = pd.crosstab(df["Class"],df["cluster"])
+print(comparison)
+
+fig, axes = plt.subplots(1,2, figsize =(12,15))
+sns.scatterplot(
+    data = df,
+    x= "Compactness",
+    y="Coefficient",
+    hue = "Class",
+    palette= "Set1",
+    s = 70,
+    ax = axes[0]
+)
+axes[0].set_title("Original Seed Classes")
+axes[0].set_xlabel("Compactness")
+axes[0].set_ylabel("Coefficient")
+
+sns.scatterplot(
+    data = df,
+    x= "Compactness",
+    y="Coefficient",
+    hue = "cluster",
+    palette= "Set1",
+    s = 70,
+    ax = axes[1]
+)
+axes[1].set_title("K-Means Cluster")
+axes[1].set_xlabel("Compactness")
+axes[1].set_ylabel("Coefficient")
+
+plt.tight_layout()
+plt.show()
